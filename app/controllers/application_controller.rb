5f12145c0dd42c0abd10df10d25c4877e0f9d251
nadhem maaloul <nadhemm1@gmail.com>
2019-11-09 12:29:06 +0100

    frontend fixes, add datatable, beautiful IU

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,50 +1,49 @@
 require 'money/bank/currencylayer_bank'
 
 class ApplicationController < Sinatra::Base
-	# This configuration part will inform the app where to search for the views and from where it will serve the static files
-  	configure do
-    	set :views, "app/views"
-    	set :public_dir, "public"
-  	end
+  # This configuration part will inform the app where to search for the views and from where it will serve the static files
+  configure do
+    set :views, "app/views"
+    set :public_dir, "public"
+  end
 
-  	get '/' do
-   		erb :convert
-  	end
+  get '/' do
+    erb :convert
+  end
 
-		get '/convert' do
-			erb :convert
-		end
+  get '/convert' do
+		@input_amount = 1
+    erb :convert
+  end
 
-		post '/convert' do
-			mclb = Money::Bank::CurrencylayerBank.new
-			mclb.access_key = '125aad7f1a6b30553506d370d6310216'
-			mclb.update_rates(true)
+  post '/convert' do
+    mclb = Money::Bank::CurrencylayerBank.new
+    mclb.access_key = '125aad7f1a6b30553506d370d6310216'
+    mclb.update_rates(true)
 
-			mclb.cache = 'mclb_cache_file'
-			Money.default_bank = mclb
+    mclb.cache = 'mclb_cache_file'
+    Money.default_bank = mclb
 
-			@from_currency = "USD"
-			@to_currency = "EUR"
-			@input_amount = params[:input_amount]
-			@output_amount = Money.new(100, 'USD').exchange_to('EUR').to_f
-			puts "conv: #{@output_amount}"
-			@conversion = Conversion.create(
-					:from_currency      => @from_currency,
-					:to_currency       => @to_currency,
-					:input_amount      => @input_amount.to_f,
-					:output_amount      => @output_amount.to_f,
-					:created_at => Time.now
-			)
-			erb :convert
-  	end
-		get '/history' do
-			@conversations = Conversion.all()
-			for conversation in @conversations do
-				puts "here : #{conversation.input_amount}"
-			end
-			erb :history
-		end
+    @from_currency = params[:from_currency]
+    @to_currency = params[:to_currency]
+    @input_amount = params[:input_amount].to_i
+		puts " #{@to_currency}#{@from_currency}#{@input_amount}"
+    @output_amount = Money.new(100 * @input_amount, @from_currency).exchange_to(@to_currency).to_f.round(3)
+    puts "conv: #{@output_amount}"
+    @conversion = Conversion.create(
+        :from_currency => @from_currency,
+        :to_currency => @to_currency,
+        :input_amount => @input_amount.to_f,
+        :output_amount => @output_amount.to_f,
+        :created_at => Time.now
+    )
+    erb :convert
+  end
+
+  get '/history' do
+    @conversions = Conversion.all()
+		puts @conversions.size
+    erb :history
+  end
 
 end
-
-
